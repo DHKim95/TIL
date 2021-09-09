cf58a173b695b377d6069705bcb64647748c9000
DHKim95 <tbvjdkrak2@gmail.com>
2021-09-09 21:16:22 +0900

    9/9

---
+++ study/210913/BOJ_11725.py
@@ -0,0 +1,20 @@
+N = int(input()) # 노드 개수
+graph = [[] for _ in range(N+1)]
+for i in range(N-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [0 for _ in range(N+1)]
+
+def DFS(start, graph, visited):
+    for i in graph[start]:
+        # 방문 안한거면 start가 부모
+        if visited[i] == 0:
+            visited[i] = start
+            DFS(i, graph, visited)
+
+DFS(1, graph, visited)
+
+for i in range(2, N+1):
+    print(visited[i])
