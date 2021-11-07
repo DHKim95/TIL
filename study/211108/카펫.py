3acc539f22485f9a3606e3b4ffa87d570bb5689a
DHKim95 <tbvjdkrak2@gmail.com>
2021-11-07 22:38:18 +0900

    11/7

--- study/211108/카펫.py
+++ study/211108/카펫.py
@@ -7,8 +7,7 @@ def solution(brown, yellow):
         # 나누어 진다면 높이가 나옴
         if square % i == 0:
             h = square // i
-
-            # 높이-2, 가로-2 곱한게 yellow라면 yellow 사각형이다.
+            # 높이-2, 가로-2 곱한게 yellow 라면 yellow 사각형이다.
             if (h - 2) * (i - 2) == yellow:
                 return [i, h]
 
