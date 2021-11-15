097c9d76b69776380d480818ccfed01276cef280
DHKim95 <tbvjdkrak2@gmail.com>
2021-11-15 23:12:36 +0900

    11/15

--- study/211115/단속카메라.py
+++ study/211115/단속카메라.py
@@ -1,18 +1,16 @@
 routes = [[-20,-15], [-14,-5], [-18,-13], [-5,-3]]
 
 def solution(routes):
-    print(routes)
     answer = 0
-    routes.sort(key=lambda x: (x[1], x[0]))
+    routes.sort(key=lambda x: (x[1], x[0])) # 정렬시키기
 
-    end = -30001
-    for cam in routes:
-        start = cam[0]
-        print(start, end)
-        if start <= end:
-            continue
-        answer += 1
-        end = cam[1]
+    # print(routes)
+    end = -30001 # 끝지점
+    for route in routes:
+        # 범위안에 안들었으면 카메라가 필요한거
+        if end < route[0]:
+            answer += 1
+            end = route[1]
 
     return answer
 
--- study/211115/조이스틱.py
+++ study/211115/조이스틱.py
@@ -1,6 +1,41 @@
-name = "JAN"
+name = "BBB"
 
 def solution(name):
-    pass
+    answer = 0 # 횟수 더하기
+
+    # 왼쪽이 큰지 오른쪽으로 가는게 큰지 확인해서 최솟값 더하기
+    for i in name:
+        answer += min(ord(i) - ord('A'), ord('Z') - ord(i) + 1)
+
+    # 리스트로 바꿔주고 지나간것은 A로 변환
+    name = list(name)
+    name[0] = 'A'
+
+    print(answer)
+    for i in range(len(name)):
+        # A일 경우 건너뛰기
+        if name[i] == 'A':
+            continue
+
+        # 왼쪽 오른쪽 이동 횟수
+        left, right = 1, 1
+
+        print(i, name)
+        # 왼쪽으로
+        while name[i - left] == 'A':
+            print('왼쪽', left)
+            left += 1
+
+        while i + right <= len(name) - 1 and name[i + right] == 'A':
+            print("오른쪽", right)
+            right += 1
+
+        if left < right:
+            answer += left
+        else:
+            answer += right
+        name[i] = 'A'
+
+    return answer
 
 print(solution(name))
