1ba9618241755ab0e80fa14dba43aa312502d856
DHKim95 <tbvjdkrak2@gmail.com>
2022-01-09 22:31:12 +0900

    1/9

--- study/220110/BOJ_17144.py
+++ study/220110/BOJ_17144.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 R, C, T = map(int, input().split())
 
 graph = [list(map(int, input().split())) for _ in range(R)]
@@ -5,32 +7,37 @@ graph = [list(map(int, input().split())) for _ in range(R)]
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
-time = 0
-while time < T:
-    visited = [[0] * C for _ in range(R)]
+air_machine = []
+
+for i in range(R):
+    for j in range(C):
+        if graph[i][j] == -1:
+            air_machine.append([i, j])
+
+for time in range(T):
+    arr = [[0] * C for _ in range(R)]
+
     for i in range(R):
         for j in range(C):
-            if graph[i][j] > 0:
+            # 공기청정기가 아니면
+            if graph[i][j] != -1:
                 cnt = 0
+                # 4방향 탐색
                 for direction in range(4):
-                    nx = i + dx[direction]
-                    ny = j + dy[direction]
-                    if 0 <= nx <= R and 0 <= ny <= C and graph[i][j] != -1:
-                        visited[nx][ny] += (graph[i][j] // 5)
-                        cnt += 1
-
-                graph[i][j] -= (graph[i][j] // 5) * cnt
-
-    # 확산 미세먼지 합치기
-    for i in range(R):
-        for j in range(C):
-            graph[i][j] += visited[i][j]
+                    nx, ny = i + dx[direction], j + dy[direction]
 
 
+                    if 0 <= nx < R and 0 <= ny < C:
+                        if graph[nx][ny] != -1:
+                            cnt += 1
+                            arr[nx][ny] += (graph[i][j] // 5)
 
+                arr[i][j] += (graph[i][j] - (graph[i][j] // 5) * cnt)
+            else:
+                arr[i][j] = -1
 
-    time += 1
+    graph = arr
 
+    for i in reversed(range(1, air_machine[0][0] + 1)):
+        graph[i][0] = graph[i-1][0]
 
-print(graph)
-print(visited)
