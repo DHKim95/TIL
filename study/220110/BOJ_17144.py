37eac3e073ded6390e5a2c3cd32bcb22efd422d8
DHKim95 <tbvjdkrak2@gmail.com>
2022-01-13 22:54:15 +0900

    1/13

--- study/220110/BOJ_17144.py
+++ study/220110/BOJ_17144.py
@@ -7,37 +7,35 @@ graph = [list(map(int, input().split())) for _ in range(R)]
 dx = [-1, 0, 1, 0]
 dy = [0, -1, 0, 1]
 
+dust_lst = deque()
 air_machine = []
 
 for i in range(R):
     for j in range(C):
         if graph[i][j] == -1:
-            air_machine.append([i, j])
+            air_machine.append((i, j))
+            air_machine.append((i+1, y))
+            break
 
-for time in range(T):
-    arr = [[0] * C for _ in range(R)]
-
+# 미세먼지 담기
+for _ in range(T):
     for i in range(R):
         for j in range(C):
-            # 공기청정기가 아니면
-            if graph[i][j] != -1:
-                cnt = 0
-                # 4방향 탐색
-                for direction in range(4):
-                    nx, ny = i + dx[direction], j + dy[direction]
+            if graph[i][j] > 0:
+                dust_lst.append((i, j, graph[i][j]))
 
+    while dust_lst:
+        x, y, dus = dust_lst.popleft()
+        cnt = 0
+        for direction in range(4):
+            nx = x + dx[direction]
+            ny = y + dy[direction]
+            if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] != -1:
+                graph[nx][ny] += dus // 5
+                graph[x][y] -= dus // 5
 
-                    if 0 <= nx < R and 0 <= ny < C:
-                        if graph[nx][ny] != -1:
-                            cnt += 1
-                            arr[nx][ny] += (graph[i][j] // 5)
-
-                arr[i][j] += (graph[i][j] - (graph[i][j] // 5) * cnt)
-            else:
-                arr[i][j] = -1
-
-    graph = arr
-
-    for i in reversed(range(1, air_machine[0][0] + 1)):
-        graph[i][0] = graph[i-1][0]
-
+    graph2 = [l[:] for l in graph]
+    for x in range(R):
+        for j in range(C):
+            if x == air_machine[0][0] and air_machine[0][1] < y < C:
+                graph[x][y] = graph2[x][y-1]
