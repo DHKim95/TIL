898f139251733ce40d7d19343d008dbd38e7b704
DHKim95 <tbvjdkrak2@gmail.com>
2022-01-14 22:42:35 +0900

    1/14

--- study/220110/BOJ_17144.py
+++ study/220110/BOJ_17144.py
@@ -4,38 +4,66 @@ R, C, T = map(int, input().split())
 
 graph = [list(map(int, input().split())) for _ in range(R)]
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-dust_lst = deque()
-air_machine = []
+def air_spread():
+    global graph
 
-for i in range(R):
-    for j in range(C):
-        if graph[i][j] == -1:
-            air_machine.append((i, j))
-            air_machine.append((i+1, y))
-            break
+    visited = [[0] * C for _ in range(R)]
 
-# 미세먼지 담기
-for _ in range(T):
     for i in range(R):
         for j in range(C):
             if graph[i][j] > 0:
-                dust_lst.append((i, j, graph[i][j]))
+                cnt = 0
+                move_dust = graph[i][j] // 5
 
-    while dust_lst:
-        x, y, dus = dust_lst.popleft()
-        cnt = 0
-        for direction in range(4):
-            nx = x + dx[direction]
-            ny = y + dy[direction]
-            if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] != -1:
-                graph[nx][ny] += dus // 5
-                graph[x][y] -= dus // 5
+                for direction in range(4):
+                    nx = i + dx[direction]
+                    ny = j + dy[direction]
 
-    graph2 = [l[:] for l in graph]
-    for x in range(R):
+                    if 0 <= nx < R and 0 <= ny < C and graph[nx][ny] != -1:
+                        visited[nx][ny] += move_dust
+                        cnt += 1
+
+                visited[i][j] -= (move_dust * cnt)
+
+    for i in range(R):
         for j in range(C):
-            if x == air_machine[0][0] and air_machine[0][1] < y < C:
-                graph[x][y] = graph2[x][y-1]
+            graph[i][j] += visited[i][j]
+
+def clean(m, dxdy):
+    direction = 0
+    nx = m
+    ny = 0
+    temp = 0
+    while direction < 4:
+        nx += dxdy[direction][0]
+        ny += dxdy[direction][1]
+
+        if nx == m and ny == 0:
+            return
+
+        if 0 <= nx < R and 0 <= ny < C:
+            temp, graph[nx][ny] = graph[nx][ny], temp
+        else:
+            nx -= dxdy[direction][0]
+            ny -= dxdy[direction][1]
+            direction+= 1
+
+air_machine = 0
+for i in range(R):
+    if graph[i][0] == -1:
+        air_machine = i
+        break
+
+for _ in range(T):
+    air_spread()
+
+    dxdy = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+    clean(air_machine, dxdy)
+
+    dxdy2 = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    clean(air_machine+1, dxdy2)
+
+print(sum(map(sum, graph)) + 2)
