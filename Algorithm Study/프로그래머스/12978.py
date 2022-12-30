39d69209cc0c25ecb8e8a83e94ee0babbebf9035
DHKim95 <tbvjdkrak2@gmail.com>
2022-12-31 00:56:33 +0900

    12/29 Programmers Algorithm

---
+++ Algorithm Study/프로그래머스/12978.py
@@ -0,0 +1,39 @@
+import heapq
+
+
+def solution(N, road, K):
+    def dikjstra(start):
+        q = []
+        heapq.heappush(q, (0, start))
+        distance[start] = 0
+
+        while q:
+            dist, now = heapq.heappop(q)
+
+            if distance[now] < dist:
+                continue
+
+            for i in graph[now]:
+                cost = dist + i[1]
+
+                if cost < distance[i[0]]:
+                    distance[i[0]] = cost
+                    heapq.heappush(q, (cost, i[0]))
+
+    answer = 0
+
+    min_value = 987654321
+    distance = [min_value] * (N + 1)
+    graph = [[] for _ in range(N + 1)]
+
+    for a, b, c in road:
+        graph[a].append((b, c))
+        graph[b].append((a, c))
+
+    dikjstra(1)
+
+    for i in range(1, N + 1):
+        if distance[i] <= K:
+            answer += 1
+
+    return answer
