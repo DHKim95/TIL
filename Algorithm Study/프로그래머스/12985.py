56155a1e358ecf09c2c6ef4f883172ba583c419e
DHKim95 <tbvjdkrak2@gmail.com>
2022-12-31 00:50:36 +0900

    12/23 Programmers Algorithm

---
+++ Algorithm Study/프로그래머스/12985.py
@@ -0,0 +1,15 @@
+import math
+
+
+def solution(n, a, b):
+    answer = 0
+
+    while a != b:
+        answer += 1
+        a = math.ceil(a / 2)
+        b = math.ceil(b / 2)
+
+        if a == b:
+            break
+
+    return answer
